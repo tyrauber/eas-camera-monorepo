f5e2570e3233dd3baff60658a2ed0623dbe32650
Cedric van Putten <me@bycedric.com>
2021-11-29 21:52:23 +0100

    Add parenthesis to the file url

--- apps/expo/plugins/withCameraMonorepo.js
+++ apps/expo/plugins/withCameraMonorepo.js
@@ -6,7 +6,7 @@ const {
 // See: https://github.com/expo/expo/blob/6877c1f5cdca62b395b0d5f49d87f2f3dbb50bec/packages/expo-camera/plugin/src/withCamera.ts#L18
 const wrongPath = `url "$rootDir/../node_modules/expo-camera/android/maven"`;
 // See: https://github.com/expo/expo/blob/b574deb5b399b05bbd16a1fd9d6232e6d58c27e8/packages/expo-camera/plugin/src/withCamera.ts#L23
-const rightPath = `url new File(["node", "--print", "require.resolve('expo-camera/package.json')"].execute(null, rootDir).text.trim(), "../android/maven")`;
+const rightPath = `url(new File(["node", "--print", "require.resolve('expo-camera/package.json')"].execute(null, rootDir).text.trim(), "../android/maven"))`;
 
 function withCameraMonorepo(config) {
   return withProjectBuildGradle(config, (config) => {
